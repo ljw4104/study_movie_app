caf9a4d3d441b633837845852e92afb376a95930
test <test>
2022-09-16 18:02:38 +0900

    업뎃 916

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//state 동적데이터를 와랄랄라
+
+// function App() {
+//   return <div>함수 컴포넌트</div>;
+// }
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: -1,
+    };
+  }
+  up = () => {
+    this.setState((state) => {
+      return { count: state.count + 1 };
+    });
+  };
+  down = () => {
+    this.setState((state) => {
+      return { count: state.count - 1 };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>카운터 : {this.state.count}</h1>
+        <button onClick={this.up}>+1</button>
+        <span> </span>
+        <button onClick={this.down}>-1</button>
+      </div>
+    );
+  }
 }
 
 export default App;
